b665cd71ff294f05f91bc9eeb48447531b85c9cc
testeGit <thiaagocia23@gmail.com>
2024-03-25 16:52:01 -0300

    Primeira versão

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const sequelize = require('./sequelize');
+const pessoasRouter = require('./routes/pessoas');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/pessoas', pessoasRouter);
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
